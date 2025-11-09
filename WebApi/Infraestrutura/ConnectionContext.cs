986b5b66847cee6569d22a16ff01d072fd5cc11d
Aletmf <trause.alessandro@gmail.com>
2025-11-09 16:06:24 -0300

    API Employee V1

---
+++ WebApi/Infraestrutura/ConnectionContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Model;
+
+namespace WebApi.Infraestrutura
+{
+    public class ConnectionContext : DbContext // Herdar para fazer conexão no banco
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        // Aqui sobrescreveremos o método OnConfiguring do DbContext, para informar os dados da conexão com nosso banco!
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql
+            (
+                "Server= localhost;" +
+                "Port=5432;Database=employee_sample;" +
+                "User Id=postgres;" +
+                "Password=alessandro1408;"
+            );
+            
+                
+            
+
+    }
+}
---
+++ WebApi/Model/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿// Agora implantaremos o repository, para deixar o código mais profissional separando regra de negócio / arquitetura de bd
+
+namespace WebApi.Model
+{
+    public interface IEmployeeRepository
+    {
+        void Add(Employee employee);
+        List<Employee> Get();
+    }
+}
